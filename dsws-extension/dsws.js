14bb01457de4a1cd7e053450e9e98e61fcb54015
Lucas <lucnun>
2023-09-18 16:05:28 -0300

    adds chrome api compatibility
    Signed-off-by: Lucas <lucnun>

--- dsws-extension/dsws.js
+++ dsws-extension/dsws.js
@@ -1,6 +1,13 @@
-browser.browserAction.onClicked.addListener(() => {
-    let creating = browser.tabs.create({
-      url: "home.html",
-    });
-    creating.then(onCreated, onError);
-  });
+if (typeof InstallTrigger !== 'undefined')
+{
+	browser.browserAction.onClicked.addListener(() => {
+	    let creating = browser.tabs.create({
+	      url: "home.html",
+	    });
+	  });
+ } else 
+ {
+	chrome.browserAction.onClicked.addListener(function(activeTab) {
+		chrome.tabs.create({ url: "home.html" });
+	});
+ }
--- dsws-extension/dsws.ts
+++
@@ -1,7 +0,0 @@
-browser.browserAction.onClicked.addListener(() => {
-    let creating = browser.tabs.create({
-      url: "https://example.org",
-    });
-    creating.then(onCreated, onError);
-  });
-  
