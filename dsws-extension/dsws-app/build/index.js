00cf7b822ad5d7a2b54dbcb27bb568d83b15c637
GustavoNogai <gustavo.nogaisaito@usp.br>
2023-10-02 18:26:03 -0300

    Get "asset_id" from "url" given by user input

--- dsws-extension/dsws-app/build/index.js
+++ dsws-extension/dsws-app/build/index.js
@@ -2,6 +2,11 @@
 const dropContainer = document.getElementById("dropContainer");
 const dropContainerText = document.getElementById("dropContainerText");
 const fileInput = document.getElementById("fileInput");
+
+var obj;
+var asset_ids = [];
+const urlInput = document.getElementById("urlInput");
+
 // Se algum arquivo for enviado pelo botão mostrar também no container.
 fileInput.addEventListener("input", () => {
     if (dropContainer && fileInput.files) {
@@ -11,13 +16,14 @@ fileInput.addEventListener("input", () => {
         var text="";
         zip.loadAsync(file).then(function(zip) {
         	Object.keys(zip.files).forEach(function(file){
+                text = text + "<strong>Arquivo selecionado:</strong>&nbsp;" + file + "<br>";
         		zip.files[file].async('string').then(function (fileData) {
-        			text= text+"<strong>Arquivo selecionado:</strong>&nbsp;" + file+"<br>";
-        			dropContainer.innerHTML=text;
+                    // Guarda "routes2.json"
+                    if (file === "routes2.json") obj = JSON.parse(fileData);
         		})
         	})
+            dropContainer.innerHTML=text;
         })
-        
     }
 });
 // Ao arrastar um arquivo ao container não abrir uma nova aba.
@@ -39,5 +45,40 @@ if (dropContainer) {
             }
         }
     };
-}
-;
+};
+
+// Busca o "asset_id" do arquivo "file" desejado no JSON "obj".
+function findAssetID(obj, file) {
+  // Itera o JSON "obj" e obtém suas chaves e valores.
+  Object.entries(obj).some(([key, value]) => {
+    // Se for uma "response" quer dizer que já iterou pelo pai e não é a "file", então para.
+    if (key === "response") return false;
+    // Se achar o arquivo guarda seu "asset_id" acessando sua "response" filha.
+    else if (key === file) {
+      console.log(key + " " + value["response"]["asset_id"]);
+      asset_ids.push(value["response"]["asset_id"]);
+      return true;
+    }
+    // Se houver objetos filhos faz a recursão da função.
+    else if (value) return findAssetID(value, file);
+    // Se não achar e não tiver filhos para.
+    else return false;
+  });
+};
+
+// Função que roda ao clicar no botão para teste.
+function urlButtonClick(){
+    let file = urlInput.value;
+    if (!obj) {
+        alert("Primeiro de upload do(s) arquivos! \nOu espere um pouco serem processados!");
+        return -1;
+    }
+    
+    if (file.length === 0) file = "alignment.html";
+    findAssetID(obj, file);
+
+    if (asset_ids.length > 0) alert("asset_id(s) de " + file + ":\n" + asset_ids.join("\n"));
+    else alert("Url não encontrado!");
+
+    asset_ids = [];
+};
