718e4849d9e2697bd68b82ccd9407d84a7d7f733
JGBSouza <joaosouzaaa12@usp.br>
2023-10-03 15:59:09 -0300

    add inputLink.js to index.js

--- dsws-extension/dsws-app/build/index.js
+++ dsws-extension/dsws-app/build/index.js
@@ -2,12 +2,36 @@
 const dropContainer = document.getElementById("dropContainer");
 const dropContainerText = document.getElementById("dropContainerText");
 const fileInput = document.getElementById("fileInput");
+var fileButton = document.getElementById('fileButton');
+
+function changeContainerText(file){
+    dropContainerText.innerHTML = '<strong>Arquivo selecionado:</strong>&nbsp;' + 
+    file.files[0].name + 
+    '<p id="dropContainerText">' +                     
+    '<a id="fileButton">browse another file</a>' +
+    '<input type="file" id="fileInput"></p>';
+
+    fileButton = document.getElementById('fileButton');
+
+    fileButton.addEventListener('click', () => {
+        // Acionar o clique no input de arquivo
+        fileInput.click();
+    });
+
+}
 // Se algum arquivo for enviado pelo botão mostrar também no container.
 fileInput.addEventListener("input", () => {
     if (dropContainer && fileInput.files) {
-        dropContainer.innerHTML = "<strong>Arquivo selecionado:</strong>&nbsp;" + fileInput.files[0].name;
+        changeContainerText(fileInput);
     }
 });
+
+//Se algum click for dado no link "browse" aciona o fileInput
+fileButton.addEventListener('click', () => {
+    // Acionar o clique no input de arquivo
+    fileInput.click();
+});
+
 // Ao arrastar um arquivo ao container não abrir uma nova aba.
 if (dropContainer) {
     dropContainer.ondragover = function (e) {
@@ -18,7 +42,7 @@ if (dropContainer) {
         // Se algum arquivo for arrastado ao container mostrar seu nome no próprio container.
         if (e.dataTransfer) {
             console.log(e.dataTransfer.files[0].name);
-            dropContainerText.innerHTML = "<strong>Arquivo selecionado:</strong>&nbsp;" + e.dataTransfer.files[0].name;
+            changeContainerText(e.dataTransfer);
             // Se algum arquivo for arrastado ao container mostrar também no input.
             if (fileInput) {
                 const dT = new DataTransfer();
