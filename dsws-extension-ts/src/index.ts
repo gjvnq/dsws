c5ea21532112eb9b0634f833dd1a4fd6aee68944
JGBSouza <joaosouzaaa12@usp.br>
2023-12-09 20:24:53 -0300

    refactor the index.ts functions

--- dsws-extension-ts/src/index.ts
+++ dsws-extension-ts/src/index.ts
@@ -1,162 +1,162 @@
-var DswsFilename: String = "";
+var DswsFilename :String = "";
+let dropContainer :HTMLElement;
+let dropContainerText :HTMLElement;
+let fileInput :HTMLInputElement;
+let fileButton :HTMLElement;
+let object :Record<string, any> | undefined;
+let asset_ids :string[] = [];
+let fileContainer : HTMLElement;
 
-let dropContainer = document.getElementById("dropContainer") as HTMLElement;
-let urlInput = document.getElementById("urlInput") as HTMLInputElement;
+(navigator as Navigator).serviceWorker.addEventListener('message', (event) => {a(event)});
+const mainIframe :HTMLIFrameElement = document.getElementById("main-iframe") as HTMLIFrameElement;
 
-let dropContainerText = document.getElementById("dropContainerText") as HTMLElement;
-let fileInput = document.getElementById("fileInput") as HTMLInputElement;
-let fileButton = document.getElementById('fileButton') as HTMLElement;
-
-let obj: Record<string, any> | undefined;
-let asset_ids: string[] = [];
-
-window.onload = function(){
-    dropContainer = document.getElementById("dropContainer") as HTMLElement;
-    urlInput = document.getElementById("urlInput") as HTMLInputElement;
-    dropContainerText = document.getElementById("dropContainerText") as HTMLElement;
-    fileInput = document.getElementById("fileInput") as HTMLInputElement;
-    fileButton = document.getElementById('fileButton') as HTMLElement;
-
-    if (dropContainer && urlInput && dropContainerText && fileInput && fileButton){
-        fileButton.addEventListener('click', () => {
-            fileInput.click();
-    });
-        fileInput.addEventListener("input", onFileInput);
-        dropContainer.addEventListener("dragover", (e) => { e.preventDefault() });
-        dropContainer.addEventListener("drop", (e) => { onDrop(e) });
-        
-    }
-    else {
-        alert("Página carregada incorretamente, favor recarregá-la!");
-    }
-}
-
-function onDrop(e: DragEvent) {
-    e.preventDefault();
-    if (e.dataTransfer) {
-        console.log((e.dataTransfer as DataTransfer).files[0].name);
-        // Se algum arquivo for arrastado ao container mostrar seu nome no próprio container.
-        changeContainerText(e.dataTransfer);
-        // Le o arquivo arrastado
-        let dT = new DataTransfer();
-        dT.items.add((e.dataTransfer as DataTransfer).files[0]);
-        // Se algum arquivo for arrastado ao container mostrar também no input.
-        if (fileInput) (fileInput as HTMLInputElement).files = dT.files;
-        // Unzipa o .dsws
-        let file = dT.files[0];
-        DswsFilename = file!.name;
-        console.log('file', file);
-        (navigator as Navigator).serviceWorker!.controller!.postMessage({'action': 'openDswsFile', 'file': file})
-
-    }
-}
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello2");
-    const el = document.getElementById("fileInput") as HTMLInputElement;
-    el.disabled = false;
-    el.addEventListener('change', async (e) => {
-        console.log("Hello3a");
-        console.log(e);
-        let file = el.files!.item(0);
-        DswsFilename = file!.name;
-        console.log('file', file);
-        (navigator as Navigator).serviceWorker!.controller!.postMessage({'action': 'openDswsFile', 'file': file})
-    })
-});
-
-(navigator as Navigator).serviceWorker.addEventListener('message', (event) => {
-    // event is a MessageEvent object
-    console.log(`The service worker sent me a message: ${event.data}`);
-    const message = event.data;
-    let fileContent = document.getElementById("FileContainer") as HTMLElement;
-
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.event == 'dswsReady') {
-        const el = document.getElementById("main-iframe") as HTMLIFrameElement;
-        fileContent.style.display = 'none';
-        // Idiomas preferidos do usuário
-        let languages = navigator.languages;
-        console.log("Idiomas preferidos: " + languages);
-        // Idiomas preferidos mais string vazia, levando ao "index" se não achar nenhuma
-        let langs: string[] = [];
-        languages.forEach(function (l){
-            langs.push(l);
-        })
-        langs.push("");
-        // Esconde o iframe enquanto procura os idiomas preferidos.
-        el.style.display = "none";
-        // Função recursiva para procurar
-        testLanguage(el, langs, 0);
+        const url = chrome.runtime.getURL(DswsFilename+"/");
+        fileContainer.style.display = 'none';
+        mainIframe.src = url;
     }
 });
 
+// Calls $initializePage while window is loading.
+window.onload = function() { initializePage(); }
 
-function testLanguage(iframe :HTMLIFrameElement, languages :string[], index :number){
-    var url = chrome.runtime.getURL(DswsFilename+"/"+languages[index]);
-    console.log(url);
-    iframe.src = url;
-    iframe.onload = function(){
-        let page;
-        try {
-            // Se o iframe carregar uma pagina existente, ou seja, achar o idioma
-            page = (iframe.contentWindow?.document || iframe.contentDocument) as Document;
-            console.log(page.body.childNodes.length);
-            if (languages[index] === "") console.log("Conseguiu carregar o iframe padrão (index)!");
-            else console.log("Conseguiu carregar o iframe em "+ languages[index] +"!");
-            // Mostra o iframe carregado
-            iframe.style.display = "block";
-            return;
-        } catch (error) {
-            // Se não achar o idioma procura a próxima linguagem
-            console.log("Não encontrou o idioma "+languages[index]+", procurando o próximo!");
-            testLanguage(iframe, languages, ++index);
+// $initializePage retrieve the HTML Elements and add the necessary 
+// EventsListener's.
+function initializePage() :void {
+    initializeElements();
+    if (correctlyInitialized()) {
+        addEventsListeners();
+    } else {
+        alert("Page initialized incorrectly, please reload it!");
+        location.reload();
+    }
+}
+
+// $initializeElements function initialize the HTML Elements from the HTML file.
+function initializeElements() :void{
+    dropContainer = document.getElementById("drop-container") as HTMLElement;
+    dropContainerText = document.getElementById("drop-container-text") as HTMLElement;
+    fileInput = document.getElementById("file-input") as HTMLInputElement;
+    fileButton = document.getElementById("file-button") as HTMLElement;
+    fileContainer = document.getElementById("file-container") as HTMLElement;
+}
+
+// $correctlyInitialized function checks if the HTML Elements were 
+// Correctly initialized.
+function correctlyInitialized() :Boolean {
+    if (dropContainer && dropContainerText && fileInput && fileButton){
+        return true;
+    }
+    return false;
+}
+
+// $addEventListeners function handle adding the @fileInput, @fileButton and
+// @dropContainer eventsListeners to check when an event will occur.
+function addEventsListeners() :void {
+    fileInput.addEventListener('input', onFileInput);
+    fileInput.addEventListener('change', async(e) => {onFileInput()});
+    fileButton.addEventListener('click', () => {fileInput.click();});
+    dropContainer.addEventListener('dragover', (e) => { e.preventDefault() });
+    dropContainer.addEventListener('drop', (e) => { onDrop(e) });
+}
+
+// $onFileInput function handle file input event, if a file was uploaded,
+// Check if there is a file and if its not a null object and then navigate
+// to that file.
+function onFileInput() :void {
+    if (fileInput.files && fileInput.files.length > 0) {
+        const transferedFile :File | null = fileInput.files.item(0);
+        if (transferedFile) {
+            navigateToFile(transferedFile);
+        } else {
+            console.log('File object is null');
+        }
+    } else {
+        console.log('No file selected');
+    }
+
+}
+
+// $onDrop function handle drag&drop @event, if a data was transfered,
+// Check the first object transfered and then navigate to that file.
+function onDrop(event :DragEvent) :void {
+    event.preventDefault();
+
+    if (event.dataTransfer) {
+        let dataTransferred = new DataTransfer();
+        dataTransferred.items.add((event.dataTransfer as DataTransfer).files[0]);
+        if (fileInput) (fileInput as HTMLInputElement).files = dataTransferred.files;
+        let transferedFile :File = dataTransferred.files[0];
+        navigateToFile(transferedFile);
+    }
+
+}
+
+// $navigateToFile function checks if @transferedFile is a dsws file and then post  
+// A message to load His content to the page if the check fails display  
+// Message in the allert window then reinitializate html elements.
+function navigateToFile(transferedFile:File) :void {
+    if(isDSWSFile(transferedFile)){
+        DswsFilename = transferedFile!.name;
+        (navigator as Navigator).serviceWorker!.controller!.postMessage({'action' :'openDswsFile', 'file' :transferedFile})
+    }
+
+    alert("Transfered file is not a \'.dsws\' extension");
+    initializePage();
+}
+
+// Chech if file name ends with '.dsws'
+function isDSWSFile(transferedFile :File) :Boolean {
+    if(!transferedFile.name.endsWith(".dsws")){
+        return false;
+    }
+    return true;
+}
+
+// $displayPage function receive a Message as an @event,
+// if the message says that 'dswsReady' select the user
+// languages to display the page. If it doesn't work
+// displays the page in the regular language.
+function displayPage(event : MessageEvent){    
+    const message = event.data;
+    
+    if (message.event == 'dswsReady') {
+        let languages = retrieveLanguages();
+        mainIframe.style.display = "none";
+        fileContainer.style.display = 'none';
+        for(let language in languages){
+            if (tryLanguage(language)){
+                break;
+            }
         }
     }
 }
 
+// $retrieveLanguages return a list off user languages that can be 
+// Used to display user page and add at the last position an empty
+// String that can be used to display files current language. 
+function retrieveLanguages(){
+    let languages :string[] = [];
+    navigator.languages.forEach((language)=>
+        languages.push(language)
+    );
+    languages.push("");
+    return languages
+}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('onMessage', message, sender, sendResponse);
-    let fileContent = document.getElementById("FileContainer") as HTMLElement;
-
-    if (message.event == 'dswsReady') {
-        const url = chrome.runtime.getURL(DswsFilename+"/");
-        console.log(url);
-        const el = document.getElementById("main-iframe") as HTMLIFrameElement;
-        fileContent.style.display = 'none';
-        el.src = url;
+function tryLanguage(language :string) :Boolean {
+    var url = chrome.runtime.getURL(DswsFilename+"/"+language);
+    mainIframe.src = url;
+    mainIframe.onload = function(){
+        let page;
+        try {
+            page = (mainIframe.contentWindow?.document || mainIframe.contentDocument) as Document;
+            mainIframe.style.display = "block";
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
-});
-
-
-function changeContainerText(file: any){
-    // Restante do seu código...
+    return false;
 }
-
-function unzipDSWS(dsws: any) {
-    // Restante do seu código...
-}
-
-function onFileInput() {
-    // Restante do seu código...
-}
-
-function findAssetID(obj: Record<string, any> | undefined, file: string) {
-    if (!obj) return; // Verifica se 'obj' é indefinido
-    // Restante do seu código...
-};
-
-function urlButtonClick(){
-    let file = urlInput.value;
-    if (!obj) {
-        alert("Primeiro de upload do(s) arquivos! \nOu espere um pouco serem processados!");
-        return -1;
-    }
-    
-    if (file.length === 0) file = "alignment.html";
-    findAssetID(obj, file);
-
-    if (asset_ids.length > 0) alert("asset_id(s) de " + file + ":\n" + asset_ids.join("\n"));
-    else alert("Url não encontrado!");
-
-    asset_ids = [];
-};
