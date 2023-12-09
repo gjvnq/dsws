5dc1031cf653bf784bfcb9d07da5ff40911b599c
Gustavo Nogai Saito <gustavo.nogaisaito@usp.br>
2023-12-09 11:12:41 -0300

    Add navigation bar to iframe

--- dsws-extension-ts/src/index.ts
+++ dsws-extension-ts/src/index.ts
@@ -4,28 +4,55 @@ let dropContainer = document.getElementById("dropContainer") as HTMLElement;
 
 let dropContainerText = document.getElementById("dropContainerText") as HTMLElement;
 let fileInput = document.getElementById("fileInput") as HTMLInputElement;
-let fileButton = document.getElementById('fileButton') as HTMLElement;
+let fileButton = document.getElementById("fileButton") as HTMLElement;
+
+let navBar = document.getElementById("navBar") as HTMLDivElement;
+let backBtn = document.getElementById("backBtn") as HTMLButtonElement;
+let forwardBtn = document.getElementById("forwardBtn") as HTMLButtonElement;
+let pageName = document.getElementById("pageName") as HTMLDivElement;
+
+let mainIframe = document.getElementById("main-iframe") as HTMLIFrameElement;
 
 let obj: Record<string, any> | undefined;
 let asset_ids: string[] = [];
+
+var pageLang: string = "";
+var prevPage: string;
 
 window.onload = function(){
     dropContainer = document.getElementById("dropContainer") as HTMLElement;
     dropContainerText = document.getElementById("dropContainerText") as HTMLElement;
     fileInput = document.getElementById("fileInput") as HTMLInputElement;
     fileButton = document.getElementById('fileButton') as HTMLElement;
+    
+    navBar = document.getElementById("navBar") as HTMLDivElement;
+    backBtn = document.getElementById("backBtn") as HTMLButtonElement;
+    forwardBtn = document.getElementById("forwardBtn") as HTMLButtonElement;
+    pageName = document.getElementById("pageName") as HTMLDivElement;
 
-    if (dropContainer && dropContainerText && fileInput && fileButton){
+    mainIframe = document.getElementById("main-iframe") as HTMLIFrameElement;
+
+    if (dropContainer && dropContainerText && fileInput && fileButton && navBar && backBtn && forwardBtn && mainIframe){
+
         fileButton.addEventListener('click', () => {
             fileInput.click();
-    });
+        });
         fileInput.addEventListener("input", onFileInput);
         dropContainer.addEventListener("dragover", (e) => { e.preventDefault() });
         dropContainer.addEventListener("drop", (e) => { onDrop(e) });
-        
+
+        backBtn.addEventListener("click", () => {
+            prevPage = mainIframe.contentWindow!.location.href;
+
+            mainIframe.contentWindow?.history.back();
+        });
+
+        forwardBtn.addEventListener("click", () => {
+            mainIframe.contentWindow?.history.forward();
+        });
     }
     else {
-        alert("Página carregada incorretamente, favor recarregá-la!");
+        alert("Extensão carregada incorretamente, favor recarregá-la!");
     }
 }
 
@@ -44,10 +71,10 @@ function onDrop(e: DragEvent) {
         let file = dT.files[0];
         DswsFilename = file!.name;
         console.log('file', file);
-        (navigator as Navigator).serviceWorker!.controller!.postMessage({'action': 'openDswsFile', 'file': file})
-
+        (navigator as Navigator).serviceWorker!.controller!.postMessage({'action': 'openDswsFile', 'file': file});
     }
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Hello2");
     const el = document.getElementById("fileInput") as HTMLInputElement;
@@ -71,6 +98,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (message.event == 'dswsReady') {
         const el = document.getElementById("main-iframe") as HTMLIFrameElement;
         fileContent.style.display = 'none';
+        // Show iframe's navigation bar
+        navBar.style.display = "block";
         // Idiomas preferidos do usuário
         let languages = navigator.languages;
         console.log("Idiomas preferidos: " + languages);
@@ -80,16 +109,16 @@ document.addEventListener('DOMContentLoaded', function() {
             langs.push(l);
         })
         langs.push("");
-        // Esconde o iframe enquanto procura os idiomas preferidos.
-        el.style.display = "none";
         // Função recursiva para procurar
         testLanguage(el, langs, 0);
     }
 });
 
-
 function testLanguage(iframe :HTMLIFrameElement, languages :string[], index :number){
-    var url = chrome.runtime.getURL(DswsFilename+"/"+languages[index]);
+    // Esconde o iframe enquanto procura os idiomas preferidos.
+    iframe.style.display = "none";
+    pageLang = languages[index];
+    var url = chrome.runtime.getURL(DswsFilename+"/"+pageLang);
     console.log(url);
     iframe.src = url;
     iframe.onload = function(){
@@ -98,15 +127,27 @@ function testLanguage(iframe :HTMLIFrameElement, languages :string[], index :num
             // Se o iframe carregar uma pagina existente, ou seja, achar o idioma
             page = (iframe.contentWindow?.document || iframe.contentDocument) as Document;
             console.log(page.body.childNodes.length);
-            if (languages[index] === "") console.log("Conseguiu carregar o iframe padrão (index)!");
-            else console.log("Conseguiu carregar o iframe em "+ languages[index] +"!");
+            // Change navigation bar page name
+            let cleanUrl = iframe.contentWindow!.location.href as string;
+            let urlArray = cleanUrl.split("/");
+            pageName.innerText = urlArray.slice(3).join("/");
+
+            if (pageLang === "" || pageLang === undefined) console.log("Conseguiu carregar o iframe padrão (index)!");
+            else console.log("Conseguiu carregar o iframe em "+ pageLang +"!");
             // Mostra o iframe carregado
             iframe.style.display = "block";
             return;
         } catch (error) {
             // Se não achar o idioma procura a próxima linguagem
-            console.log("Não encontrou o idioma "+languages[index]+", procurando o próximo!");
-            testLanguage(iframe, languages, ++index);
+            if (pageLang === undefined) {
+                console.log("Tentativar de voltar na primeira página!");
+                mainIframe.contentWindow!.location.href = prevPage;
+                iframe.style.display = "block";
+            }
+            else{ 
+                console.log("Não encontrou o idioma "+pageLang+", procurando o próximo!");
+                testLanguage(iframe, languages, ++index);
+            }
         }
     }
 }
